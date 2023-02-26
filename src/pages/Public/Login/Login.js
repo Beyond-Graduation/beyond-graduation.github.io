720d3b315381224987b5cdb53acf17e3a314cf00
SouravSatheesh <souravsatheesh1@gmail.com>
2023-02-26 11:52:24 +0530

    :sparkles: forgot password

--- src/pages/Public/Login/Login.js
+++ src/pages/Public/Login/Login.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Login.css";
 import { HiOutlineMail } from "react-icons/hi";
 import { BiLockAlt } from "react-icons/bi";
-import { AiFillEye } from "react-icons/ai";
+import { AiFillEye, AiOutlineClose } from "react-icons/ai";
 import { FaHome } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { isAuth } from "../../../auth/Auth";
@@ -15,6 +15,10 @@ function Login() {
   const navigate = useNavigate();
   const passToggleRef = useRef();
   const rememberRef = useRef();
+  const forgotPasswordEmailRef = useRef();
+  const overlayCntRef = useRef();
+  const overlayRef = useRef();
+  const [forgotOverlay, setForgotOverlay] = React.useState(true);
 
   const [{ userData, userId }, dispatch] = useStateValue();
 
@@ -89,6 +93,50 @@ function Login() {
     }
   };
 
+  const resetPassword = async () => {
+    const email = forgotPasswordEmailRef.current.value;
+    if (
+      !email
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    )
+      toast.error("Invalid Email");
+    else {
+      await axios({
+        method: "post",
+        url: "user/forgot_password",
+        data: {
+          email: email,
+        },
+      })
+        .then(async (res) => {
+          if (res.status === 200) {
+            toast.success("Reset Password Link sent to registered mail")
+          }
+        })
+        .catch((err) => {
+          toast.error(err.response.data);
+        });
+    }
+  };
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (
+        overlayCntRef.current &&
+        !overlayCntRef.current.contains(e.target) &&
+        overlayRef.current.contains(e.target)
+      ) {
+        forgotPasswordEmailRef.current.value = "";
+        setForgotOverlay(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+    return () => document.removeEventListener("click", handleClick);
+  }, []);
+
   return (
     <div className="login-main d-flex align-items-center justify-content-center">
       <Link to="/">
@@ -125,7 +173,9 @@ function Login() {
             <AiFillEye className="icon-eye" onClick={togglePassword} />
           </div>
 
-          <p id="fp">Forgot Password ?</p>
+          <p id="fp" onClick={() => setForgotOverlay(true)}>
+            Forgot Password ?
+          </p>
           <label className="rem-cb d-flex align-items-center">
             <input type="checkbox" ref={rememberRef} /> Remember Me ?
           </label>
@@ -136,6 +186,35 @@ function Login() {
             Don't have an account already ?
             <Link to="/register"> Register Now.</Link>
           </p>
+        </div>
+      </div>
+      <div className="forgot-overlay" ref={overlayRef} hidden={!forgotOverlay}>
+        <div className="forgot-overlay-cnt" ref={overlayCntRef}>
+          <AiOutlineClose
+            className="overlay-close"
+            onClick={() => {
+              forgotPasswordEmailRef.current.value = "";
+              setForgotOverlay(false);
+            }}
+          />
+          <div className="forgot-overlay-head">Forgot Password</div>
+          <div className="input-cnt">
+            <input
+              type="email"
+              name="forgotPswdEmail"
+              autoComplete="off"
+              placeholder="Registered Email"
+              ref={forgotPasswordEmailRef}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") resetPassword();
+              }}
+            ></input>
+            <HiOutlineMail className="icon-email-pswd" />
+          </div>
+          <div className="forgot-btn" onClick={resetPassword}>
+            Reset Password
+          </div>
+          <p>Password reset link would be sent to your email.</p>
         </div>
       </div>
     </div>
